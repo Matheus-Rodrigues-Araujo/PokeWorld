4d360709f8bda5314e0dd01e32777a60c34d2a34
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-04-26 17:50:27 -0300

    moves and gamesindices components updated

--- src/components/ModalElements/GameIndices.vue
+++ src/components/ModalElements/GameIndices.vue
@@ -1,26 +1,39 @@
 <script setup>
+import { ref } from 'vue';
+
 const props = defineProps({
   games: Array,
 });
+
+const count = ref(8)
 </script>
 <template>
   <div class="games px-2 px-md-5 d-flex flex-column">
     <h6 class="fs-4 text-center">Games Indices</h6>
-    <ul class="games-list gap-1 p-0">
-      <li
-        v-for="game in games"
-        class="game d-flex flex-column p-1 gap-1 align-items-center justify-content-center fs-6 rounded-1"
+    <ul class="games-list p-0">
+      <li v-for="game in games.slice(0, count)"
+        class="game d-flex flex-column p-2 align-items-center justify-content-center  rounded-1"
       >
-        <p class="m-0">Index {{ game.game_index }}</p>
-        <p class="m-0">Version {{ game.version.name }}</p>
+        <p class="m-0 fs-6">Index {{ game.game_index }}</p>
+        <p class="m-0 fs-6 ">Version {{ game.version.name }}</p>
       </li>
     </ul>
+    <button
+      class="w-50 p-2 mx-auto mt-3 text-white fs-5 fw-bold border-0 rounded-1"
+      @click="count < props.games.length ? (count += 4) : (count = 8)"
+      v-if="props.games.length > 12"
+      style="background-color: var(--custom-red)"
+    >
+      {{ count < props.games.length ? "Show more games" : "Show less" }}
+    </button>
   </div>
 </template>
 <style>
-.games-list {
+
+.games-list{
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(4, 1fr);
+  gap: 4px;
 }
 .game {
   width: auto;
@@ -30,6 +43,13 @@ const props = defineProps({
 }
 
 @media screen and (max-width: 720px) {
+  .games-list {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+
+@media screen and (max-width: 500px) {
   .games-list {
     display: grid;
     grid-template-columns: repeat(1, 1fr);
--- src/components/ModalElements/Moves.vue
+++ src/components/ModalElements/Moves.vue
@@ -1,24 +1,41 @@
 <script setup>
+import { ref } from "vue";
+
 const props = defineProps({
   moves: Array,
 });
+
+const count = ref(12);
 </script>
 <template>
   <div class="moves px-2 px-md-5 d-flex flex-column">
     <h6 class="fs-4 text-center">Moves</h6>
     <ul class="moves-list gap-1 p-0">
-      <li v-for="move in props.moves" class="move fs-6 text-center rounded-1">
+      <li
+        v-for="move in props.moves.slice(0, count)"
+        class="move fs-6 text-center rounded-1"
+      >
         {{ move.move.name }}
       </li>
     </ul>
+    <button
+      class="w-50 p-2 mx-auto mt-3 text-white fs-5 fw-bold border-0 rounded-1"
+      @click="count < props.moves.length ? (count += 8) : (count = 12)"
+      v-if="props.moves.length > 20"
+      style="background-color: var(--custom-red)"
+    >
+      {{ count < props.moves.length ? "Show more moves" : "Show less" }}
+    </button>
   </div>
 </template>
 <style>
 .moves-list {
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(4, 1fr);
+  gap: 4px;
 }
 .move {
+  flex: 1;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -28,7 +45,14 @@ const props = defineProps({
   text-transform: capitalize;
 }
 
-@media screen and (max-width: 720px) {
+@media screen and (max-width: 800px) {
+  .moves-list {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+
+@media screen and (max-width: 500px) {
   .moves-list {
     display: grid;
     grid-template-columns: repeat(1, 1fr);
