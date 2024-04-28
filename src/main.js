922195db63276833360a23e8d0c3dcb96646377e
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-04-28 17:24:19 -0300

    New directories: locales & plugins

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import en from "../src/locale/en.json";
+import es from "../src/locale/es.json";
+import ptBr from "../src/locale/pt-br.json";
 
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.min.css'
-createApp(App).mount('#app')
+import "./style.css";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+
+import i18nPlugin from "./plugins/i18n";
+
+const app = createApp(App);
+app.use(i18nPlugin, {
+  en: en,
+  es: es,
+  ptBr: ptBr,
+});
+
+app.mount("#app");
